a004399a2c0c09df18dc457c179bdab1144e6c20
ramdani1202 <ramdani120208@gmail.com>
2026-09-18 09:15:25 +0700

    Update Profile.js

--- src/data/Profile.js
+++ src/data/Profile.js
@@ -5,7 +5,7 @@ export const profile = {
   bio: `I'm just an ordinary person without a very high level of formal education, but I always keep learning everything I love in the world of digital technology. My experience is fairly solid, yet I keep learning and growing. The experience from every job I've been through has shaped me into someone with strong intuition for problems — solving them and finding solutions, whether in real life or in digital systems.
 
 Warm regards, Ramdani. Looking forward to working with you.`,
-  photo: "/src/assets/images/Profil/src/assets/images/Profil/Ramdani.png",
+  photo: "/src/assets/images/Profil/Ramdani.png",
   contact: {
     linkedin: "https://www.linkedin.com/in/ramdani-63/",
     whatsapp: "083829175207",
