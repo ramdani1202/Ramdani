e1d9f340f7870e7c8b880acd227e57d10c6fee58
ramdani1202 <ramdani120208@gmail.com>
2026-09-23 12:04:08 +0700

    Add files via upload

--- src/data/Profile.js
+++ src/data/Profile.js
@@ -1,4 +1,5 @@
-import fotoProfil from "../assets/images/Profil/Ramdani.png";
+import fotoProfilBaru from "../assets/images/Profil/Ramdani2.png";
+import fotoProfilLama from "../assets/images/Profil/Ramdani.png";
 export const profile = {
   name: "Ramdani",
   role: "Creative Thinker & Problem Engineer",
@@ -6,7 +7,8 @@ export const profile = {
   bio: `I'm just an ordinary person without a very high level of formal education, but I always keep learning everything I love in the world of digital technology. My experience is fairly solid, yet I keep learning and growing. The experience from every job I've been through has shaped me into someone with strong intuition for problems — solving them and finding solutions, whether in real life or in digital systems.
 
 Warm regards, Ramdani. Looking forward to working with you.`,
-  photo:fotoProfil,
+  photo: fotoProfilBaru,
+  photoAlt: fotoProfilLama,
   contact: {
     linkedin: "https://www.linkedin.com/in/ramdani-831020221?utm_source=share_via&utm_content=profile&utm_medium=member_android",
     whatsapp: "083829975287",
